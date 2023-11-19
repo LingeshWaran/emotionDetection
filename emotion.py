77a4a1fcf9735cca1546f6d597b2d5f5c0eadd37
LingeshWaran <radhalingesh@gmail.com>
2023-11-19 22:21:28 +0530

    Update emotion.py

--- emotion.py
+++ emotion.py
@@ -18,10 +18,40 @@ def authenticate(username, password):
     return False
 
 # Streamlit app
+st.experimental_set_page_config(
+    layout="wide",
+    title="Domestic Emotion Monitoring System"
+)
+
+# Function to clear sensitive information from the session state
+def clear_session_state():
+    st.session_state['is_logged_in'] = False
+    st.session_state['username'] = None
+
+# Function to load data from CSV
+@st.cache
+def load_data(uploaded_file):
+    data = pd.read_csv(uploaded_file)
+    return data
+
+# Function to preprocess data
+def preprocess_data(df):
+    df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+    df['Date'] = df['Timestamp'].dt.date
+    df['Time'] = df['Timestamp'].dt.time
+
+    # Encode emotions using LabelEncoder
+    label_encoder = LabelEncoder()
+    df['Emotion_Encoded'] = label_encoder.fit_transform(df['Emotion'])
+
+    return df, label_encoder
+
+# Streamlit app
 def main():
     # Check if user is logged in
     if 'is_logged_in' not in st.session_state:
         st.session_state['is_logged_in'] = False
+        st.session_state['username'] = None
 
     # Display login page if not logged in
     if not st.session_state['is_logged_in']:
@@ -35,39 +65,17 @@ def main():
         if st.button("Login"):
             if authenticate(username, password):
                 st.session_state['is_logged_in'] = True
+                st.session_state['username'] = username
                 st.experimental_rerun()
             else:
                 st.error("Invalid username or password. Please try again.")
 
     # Display main content if logged in
     else:
-        st.experimental_set_page_config(
-            layout="wide",
-            title="Domestic Emotion Monitoring System"
-        )
-        st.success(f"Welcome, {username}!")
-
-        # Load the CSV file
-        @st.cache_data
-        def load_data(file_path):
-            data = pd.read_csv(file_path)
-            return data
-
-        # Preprocess the data
-        def preprocess_data(df):
-            df['Timestamp'] = pd.to_datetime(df['Timestamp'])
-            df['Date'] = df['Timestamp'].dt.date
-            df['Time'] = df['Timestamp'].dt.time
-
-            # Encode emotions using LabelEncoder
-            label_encoder = LabelEncoder()
-            df['Emotion_Encoded'] = label_encoder.fit_transform(df['Emotion'])
-
-            return df, label_encoder
+        st.success(f"Welcome, {st.session_state['username']}!")
 
         # Sidebar for file selection
-        # st.sidebar.header("Upload CSV")
-        uploaded_file = "emotion_data.csv"
+        uploaded_file = st.sidebar.file_uploader("Upload CSV", type=["csv"])
 
         # Display data
         if uploaded_file is not None:
@@ -78,11 +86,15 @@ def main():
             st.write(df)
 
             # User input for timestamp range
-            start_timestamp = st.sidebar.text_input("Select Start Timestamp(YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[0])
-            end_timestamp = st.sidebar.text_input("Select End Timestamp(YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[-1])
+            try:
+                start_timestamp = pd.to_datetime(st.sidebar.text_input("Select Start Timestamp (YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[0]))
+                end_timestamp = pd.to_datetime(st.sidebar.text_input("Select End Timestamp (YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[-1]))
+            except ValueError:
+                st.error("Invalid timestamp format. Please enter timestamps in the format: YYYY-MM-DD HH:MM:SS")
+                return
 
             # Filter data for the selected period
-            selected_data = df[(df['Timestamp'] >= pd.to_datetime(start_timestamp)) & (df['Timestamp'] <= pd.to_datetime(end_timestamp))]
+            selected_data = df[(df['Timestamp'] >= start_timestamp) & (df['Timestamp'] <= end_timestamp)]
 
             # Display emotions count for the selected period
             if not selected_data.empty:
@@ -90,7 +102,6 @@ def main():
                 filtered_counts = selected_data.groupby('Emotion')['Timestamp'].count().reset_index(name='Count')
                 fig = px.bar(filtered_counts, x='Emotion', y='Count', color='Emotion', title='Emotion Counts for the Selected Period')
                 st.plotly_chart(fig)
-
             else:
                 st.warning("No data available for the selected period.")
 
@@ -99,8 +110,12 @@ def main():
 
         # Logout option
         if st.button("Logout"):
-            st.session_state['is_logged_in'] = False
+            clear_session_state()
             st.experimental_rerun()
 
         # Add footnote to all pages
         st.markdown("<p style='text-align: center;'>This project is supported by All India Council for Technical Education (AICTE), Ministry of Education, India, Arm Education, and STMicroelectronics.<br>Developers: Charan Velavan, Ebi Manuel, Benie Jaison A T, and Akshay B<br>Mentor: M.Lingeshwaran<br>St. Joseph's College of Engineering, OMR, Chennai -119.</p>", unsafe_allow_html=True)
+
+# Run the app
+if __name__ == '__main__':
+    main()
