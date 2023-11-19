69596f4b09e45c4d4db306c22648b80c834fd7ff
AKshay <149223902+Akshay-b2369@users.noreply.github.com>
2023-11-19 20:00:17 +0530

    emotion.py

--- emotion.py
+++ emotion.py
@@ -1,72 +1,90 @@
 import streamlit as st
 import pandas as pd
 import plotly.express as px
-from streamlit.hashing import generate_password_hash, verify_password
-from st_aggrid import AgGrid
+from sklearn.preprocessing import LabelEncoder
 
-# Replace these credentials with your own
-USERNAME = 'your_username'
-PASSWORD_HASH = generate_password_hash('your_password')
+# Define a dictionary to store user credentials
+user_credentials = {
+    'user1': 'password1',
+    'user2': 'password2',
+    # Add more usernames and passwords as needed
+}
 
-# Flag to check if the user is logged in
-logged_in = False
+# Function to check if the entered credentials are valid
+def authenticate(username, password):
+    stored_password = user_credentials.get(username)
+    if stored_password is not None and stored_password == password:
+        return True
+    return False
 
-@st.cache
-def read_data():
-    # Read CSV file
-    df = pd.read_csv('emotion_data.csv')
-    return df
-
-def login():
-    global logged_in
-    if st.button("Login"):
-        username = st.text_input("Username")
-        password = st.text_input("Password", type="password")
-
-        if username == USERNAME and verify_password(password, PASSWORD_HASH):
-            logged_in = True
-            st.success("Logged in successfully!")
-
-    return logged_in
-
-def create_line_graph(df):
-    fig = px.line(df, x='Timestamp', y='Emotion', markers=True, title='Emotion vs Timestamp')
-    return fig
-
-def create_bar_graph(df):
-    fig = px.bar(df['Emotion'].value_counts().reset_index(), x='index', y='Emotion', title='Emotion Counts vs Timestamp',
-                 labels={'index': 'Emotion', 'Emotion': 'Count'})
-    dominant_emotion = df['Emotion'].value_counts().idxmax()
-    return fig, dominant_emotion
-
+# Streamlit app
 def main():
-    global logged_in
-    st.title("Emotion Analysis App")
+    st.title("Login Page")
 
-    if not logged_in:
-        logged_in = login()
+    # Input fields for username and password
+    username = st.text_input("Username:")
+    password = st.text_input("Password:", type="password")
 
-    if logged_in:
-        df = read_data()
+    # Login button
+    if st.button("Login"):
+        if authenticate(username, password):
+            st.success(f"Welcome, {username}!")
+            # Load the CSV file
+            @st.cache_data
+            def load_data(file_path):
+                data = pd.read_csv(file_path)
+                return data
 
-        # Create line graph
-        line_graph = create_line_graph(df)
+            # Preprocess the data
+            def preprocess_data(df):
+                df['Timestamp'] = pd.to_datetime(df['Timestamp'])
+                df['Date'] = df['Timestamp'].dt.date
+                df['Time'] = df['Timestamp'].dt.time
 
-        # Create bar graph
-        bar_graph, dominant_emotion = create_bar_graph(df)
+                # Encode emotions using LabelEncoder
+                label_encoder = LabelEncoder()
+                df['Emotion_Encoded'] = label_encoder.fit_transform(df['Emotion'])
 
-        # Display graphs
-        st.markdown("## Emotion vs Timestamp")
-        st.plotly_chart(line_graph, use_container_width=True)
+                return df, label_encoder
 
-        st.markdown("## Emotion Counts vs Timestamp")
-        st.plotly_chart(bar_graph, use_container_width=True)
+            # Sidebar for file selection
+            #st.sidebar.header("Upload CSV")
+            #uploaded_file = st.sidebar.file_uploader("Choose a CSV file", type=["csv"])
+            uploaded_file = "emotion_data.csv"
 
-        st.markdown(f"**Dominant Emotion:** {dominant_emotion}")
+            # Display data
+            if uploaded_file is not None:
+                df, label_encoder = preprocess_data(load_data(uploaded_file))
 
-        # Display AgGrid table
-        st.markdown("## Emotion Data Table")
-        AgGrid(df)
+                # Display raw data
+                st.subheader("Preprocessed Data")
+                st.write(df)
 
-if __name__ == "__main__":
+                # User input for timestamp range
+                start_timestamp = st.sidebar.text_input("Select Start Timestamp(YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[0])
+                end_timestamp = st.sidebar.text_input("Select End Timestamp(YYYY-MM-DD HH:MM:SS)", df['Timestamp'].iloc[-1])
+
+                # Filter data for the selected period
+                selected_data = df[(df['Timestamp'] >= pd.to_datetime(start_timestamp)) & (df['Timestamp'] <= pd.to_datetime(end_timestamp))]
+
+                # Display emotions count for the selected period
+                if not selected_data.empty:
+                    st.subheader(f"Emotion Counts for Period {start_timestamp} to {end_timestamp}")
+                    filtered_counts = selected_data.groupby('Emotion')['Timestamp'].count().reset_index(name='Count')
+                    fig = px.bar(filtered_counts, x='Emotion', y='Count', color='Emotion', title='Emotion Counts for the Selected Period')
+                    st.plotly_chart(fig)
+
+                else:
+                    st.warning("No data available for the selected period.")
+
+            else:
+                st.info("Please upload a CSV file.")
+        else:
+            st.error("Invalid username or password. Please try again.")
+
+if __name__ == '__main__':
     main()
+
+
+
+
